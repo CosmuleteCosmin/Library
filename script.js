4b0c7ff13bf6f31679f094b7e43c13920ad632f8
Cosmulete Ion-Cosmin <ion.cosmulete@stud.acs.upb.ro>
2026-05-07 19:26:26 +0300

    Added css to the page

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 const myLibrary = [];
 
-function Book(name, author, id){
-    this.name = name;
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
-    this.id = id;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary(name, author){
-    myLibrary.add(Book(name, author, crypto.randomUUID()));
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    displayBooks();
 }
 
-function displayBooks(){
-    for(let i = 0; i < myLibrary.length; i++){
-        
-    }
+function displayBooks() {
+    const container = document.querySelector('#library-container');
+    if (!container) return;
+    
+    container.innerHTML = '';
+
+    myLibrary.forEach((book) => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        bookCard.innerHTML = `
+            <h3>${book.title}</h3>
+            <p><strong>Autor:</strong> ${book.author}</p>
+            <p><strong>Pagini:</strong> ${book.pages}</p>
+            <p><strong>Statut:</strong> ${book.read ? 'Citită' : 'Necitită'}</p>
+        `;
+
+        container.appendChild(bookCard);
+    });
 }
+
+const button = document.querySelector("#new-book-btn");
+
+button.addEventListener("click", () => {
+
+});
+
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+addBookToLibrary('1984', 'George Orwell', 328, false);
+addBookToLibrary('The Little Prince', 'Antoine de Saint-Exupéry', 96, true);c
